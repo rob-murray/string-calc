b5c388958d6e9fc3fe23d5689f1052022bc8953b
Rob Murray <robmurray17@gmail.com>
2015-07-13 13:19:09 +0100

    Add multiplication test. Not implemented.

--- test/test_calc.rb
+++ test/test_calc.rb
@@ -14,6 +14,11 @@ class TestCalculator < Minitest::Test
     assert_equal 10, calc("2 PLUS 5 MINUS 1 PLUS 4")
   end
 
+  def test_another_complex_expression
+    skip "not yet"
+    assert_equal 5, calc("2 TIMES 4 MINUS 3")
+  end
+
   def test_more_complex_expression
     skip "not yet"
     assert_equal 10, calc("10 DIV 2 PLUS 6 MINUS 1")
