e5dda0ad9e3db918b32a7a093b1aef847255a060
Rob Murray <robmurray17@gmail.com>
2016-05-13 14:48:05 +0100

    Add spec for order of presedence

--- test/test_calc.rb
+++ test/test_calc.rb
@@ -21,4 +21,12 @@ class TestCalculator < Minitest::Test
   def test_more_complex_expression
     assert_equal 10, calc("10 DIV 2 PLUS 6 MINUS 1")
   end
+
+  def test_precedence_ascending
+    assert_equal 23, calc("4 TIMES 5 PLUS 3")
+  end
+
+  def test_order_of_precedence_decending
+    assert_equal 23, calc("3 PLUS 4 TIMES 5")
+  end
 end
